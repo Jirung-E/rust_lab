b4d33ae1bf696b3353f415d8da396488d0fa8d70
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-07-27 16:25:29 +0900

    accept는 순서대로

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,22 @@
-#[tokio::main]
-pub async fn mpsc_server(num_clients: u32) {
-    use std::sync::mpsc;
-
-    let (sender, receiver) = mpsc::channel::<String>();
-
-    for i in 0..num_clients {
-        let sender = sender.clone();
-        tokio::spawn(async move {
-            loop {
-                sender.send(format!("Hello from client {}", i)).unwrap();
-            }
-        });
-    }
-
-    {
-        let timer = std::time::Instant::now();
-
-        let mut count = 0;
-        for _ in receiver {
-            count += 1;
-            if count % 10000000 == 0 {
-                let elapsed = timer.elapsed();
-                println!("mpsc - Received {} messages in {:?}", count, elapsed);
-                // break;
-            }
-        }
-    }
-}
+use tokio::net::TcpListener;
 
 #[tokio::main]
-pub async fn lfqueue_server(num_clients: u32) {
-    // use lockfree::queue::Queue;
-    use crossbeam::queue::SegQueue as Queue;
-    use std::sync::Arc;
+pub async fn listener_accept_test() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
 
-    let queue: Arc<Queue<String>> = Arc::new(Queue::new());
-
-    for i in 0..num_clients {
-        let queue = queue.clone();
-        tokio::spawn(async move {
-            loop {
-                queue.push(format!("Hello from client {}", i));
-            }
-        });
-    }
-
-    {
-        let timer = std::time::Instant::now();
-
-        let mut count = 0;
-        loop {
-            if let Some(_) = queue.pop() {
-                count += 1;
-                if count % 10000000 == 0 {
-                    let elapsed = timer.elapsed();
-                    println!("lfqueue - Received {} messages in {:?}", count, elapsed);
-                    // break;
+    let mut counter = 0;
+    loop {
+        match listener.accept().await {
+            Ok(_) => {
+                counter += 1;
+                if counter & 1 == 0 {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
                 }
+                println!("counter: {}", counter);
+            }
+            Err(e) => {
+                println!("Failed to accept connection: {:?}", e);
             }
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn mpsc_test() {
-        mpsc_server(10);
-    }
-
-    #[test]
-    fn lfqueue_test() {
-        lfqueue_server(10);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,5 @@
-use rust_lab::mpsc_server;
-// use rust_lab::lfqueue_server;
+use rust_lab::listener_accept_test;
 
 fn main() {
-    let num_clients = 10;
-
-    mpsc_server(num_clients);
-    // lfqueue_server(num_clients);
-    
-    // {
-        // use std::thread;
-        // let mpsc_handle = thread::spawn(move || {
-        //     mpsc_server(num_clients);
-        // });
-        // let lfqueue_handle = thread::spawn(move || {
-        //     lfqueue_server(num_clients);
-        // });
-
-        // mpsc_handle.join().unwrap();
-        // lfqueue_handle.join().unwrap();
-    // }
+    listener_accept_test();
 }
