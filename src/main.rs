16044b3e31f4e76f556cb37acfecb1cb0d55db10
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-08-21 13:27:21 +0900

    가변크기 serialize

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
-#[derive(serde::Serialize, serde::Deserialize)]
 struct Packet {
     size: u16,
     data: Vec<u8>,
 }
 
+impl Packet {
+    fn new(data: &[u8]) -> Self {
+        Self {
+            size: data.len() as u16 + 2,
+            data: data.to_vec(),
+        }
+    }
+
+    fn as_bytes(&self) -> Vec<u8> {
+        let mut bytes = bincode::serialize(&self.size).unwrap();
+        bytes.extend_from_slice(&self.data);
+        bytes
+    }
+}
+
 
 fn main() {
-    let packet = Packet {
-        size: 16,
-        data: (0..16).collect(),
-    };
+    let packet = Packet::new(&(0..16).collect::<Vec<u8>>());
 
-    println!("{:?}", bincode::serialize(&packet).unwrap());
-    // [16, 0, 16, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    // u16, u64(usize), data
+    println!("{:?}", packet.as_bytes());
+    // [18, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+    // u16, data
     // little endian
 }
