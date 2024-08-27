9e4883974595cfcc24a7d0d1b386f6dd84152131
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-08-27 20:00:36 +0900

    std::thread::spawn vs tokio::spawn - 스레드가 남는다면 둘다 스레드를 생성한다.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,43 @@
-use tokio::net::TcpListener;
+use futures::future::join_all;
 
 #[tokio::main]
 pub async fn listener_accept_test() {
-    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let h1 = tokio::spawn(async {
+        let start = tokio::time::Instant::now();
 
-    let mut counter = 0;
-    loop {
-        match listener.accept().await {
-            Ok(_) => {
-                counter += 1;
-                if counter & 1 == 0 {
-                    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
-                }
-                println!("counter: {}", counter);
-            }
-            Err(e) => {
-                println!("Failed to accept connection: {:?}", e);
-            }
+        for _ in 0..1000000 {
+            let _result = random_calc(5, 2);
         }
+
+        println!("Time elapsed: {:?}", start.elapsed());
+    });
+
+    let h2 = tokio::spawn(async {
+        let start = tokio::time::Instant::now();
+
+        for _ in 0..1000000 {
+            let _result = random_calc(5, 2);
+        }
+
+        println!("Time elapsed: {:?}", start.elapsed());
+    });
+    
+    join_all([h1, h2]).await;
+}
+
+
+fn random_calc(a: i32, b: i32) -> i32 {
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+
+    let op = ["+", "-", "*", "/"];
+    let op = *op.choose(&mut thread_rng()).unwrap();
+
+    match op {
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        "/" => a / b,
+        _ => panic!("Invalid operator"),
     }
 }
