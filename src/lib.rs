06570aaea002599f91398b3e2a4ca3ec70010b94
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-09-04 15:34:52 +0900

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,110 +1,34 @@
-#[tokio::main]
-pub async fn mpsc_server(num_clients: u32) {
-    use std::sync::mpsc;
+use lockfree::stack::Stack;
+use lockfree::queue::Queue;
+use std::thread;
+use std::sync::Arc;
 
-    let (sender, receiver) = mpsc::channel::<String>();
+const MAX_PUSHES: usize = 10_000_000;
 
-    for i in 0..num_clients {
-        let sender = sender.clone();
-        tokio::spawn(async move {
-            loop {
-                sender.send(format!("Hello from client {}", i)).unwrap();
+pub fn run(threads: usize) {
+    let stack = Arc::new(Stack::new());
+    
+    let start = std::time::Instant::now();
+    
+    let handles = (0..threads).map(|i| {
+        let stack = stack.clone();
+        thread::spawn(move || {
+            for _ in 0..MAX_PUSHES/threads {
+                stack.push(i);
             }
-        });
+        })
+    }).collect::<Vec<_>>();
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    {
-        let timer = std::time::Instant::now();
+    println!("{} threads: {} ms", threads, start.elapsed().as_millis());
 
-        let mut count = 0;
-        for _ in receiver {
-            count += 1;
-            if count % 10000000 == 0 {
-                let elapsed = timer.elapsed();
-                println!("mpsc - Received {} messages in {:?}", count, elapsed);
-                // break;
-            }
-        }
-    }
-}
-
-#[tokio::main]
-pub async fn tokio_mpsc_server(num_clients: u32) {
-    use tokio::sync::mpsc;
-
-    let (sender, mut receiver) = mpsc::unbounded_channel();
-
-    for i in 0..num_clients {
-        let sender = sender.clone();
-        tokio::spawn(async move {
-            loop {
-                sender.send(format!("Hello from client {}", i)).unwrap();
-            }
-        });
+    let mut count = 0;
+    while let Some(_) = stack.pop() {
+        count += 1;
     }
 
-    {
-        let timer = std::time::Instant::now();
-
-        let mut count = 0;
-        loop {
-            if let Some(_) = receiver.recv().await {
-                count += 1;
-                if count % 10000000 == 0 {
-                    let elapsed = timer.elapsed();
-                    println!("tokio_mpsc - Received {} messages in {:?}", count, elapsed);
-                    // break;
-                }
-            }
-        }
-    }
-}
-
-#[tokio::main]
-pub async fn lfqueue_server(num_clients: u32) {
-    // use lockfree::queue::Queue;
-    use crossbeam::queue::SegQueue as Queue;
-    use std::sync::Arc;
-
-    let queue: Arc<Queue<String>> = Arc::new(Queue::new());
-
-    for i in 0..num_clients {
-        let queue = queue.clone();
-        tokio::spawn(async move {
-            loop {
-                queue.push(format!("Hello from client {}", i));
-            }
-        });
-    }
-
-    {
-        let timer = std::time::Instant::now();
-
-        let mut count = 0;
-        loop {
-            if let Some(_) = queue.pop() {
-                count += 1;
-                if count % 10000000 == 0 {
-                    let elapsed = timer.elapsed();
-                    println!("lfqueue - Received {} messages in {:?}", count, elapsed);
-                    // break;
-                }
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn mpsc_test() {
-        mpsc_server(10);
-    }
-
-    #[test]
-    fn lfqueue_test() {
-        lfqueue_server(10);
-    }
+    println!("  count: {}", count);
 }
