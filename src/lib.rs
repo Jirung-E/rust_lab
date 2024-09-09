d2775c901d18b98998cc5b3a640f3f7840241075
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-09-10 01:59:02 +0900

    너무 느리다

---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+use bytemuck::{Pod, Zeroable};
+
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, PartialEq, Pod, Zeroable)]
+pub struct Player {
+    pub id: u32,
+    pub translation: gmm::Float3, 
+    pub rotation: gmm::Float4, 
+    pub anim_index: u32, 
+    pub anim_timer: f32, 
+}
+
+impl Player {
+    pub fn new(
+        id: u32, 
+        translation: gmm::Float3, 
+        rotation: gmm::Float4, 
+        anim_index: u32, 
+        anim_timer: f32, 
+    ) -> Self {
+        Self {
+            id, 
+            translation, 
+            rotation, 
+            anim_index, 
+            anim_timer, 
+        }
+    }
+    
+    pub fn as_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(std::mem::size_of::<Player>());
+        bytes.extend_from_slice(&self.id.to_be_bytes());
+        bytes.extend_from_slice(&self.translation.x.to_be_bytes());
+        bytes.extend_from_slice(&self.translation.y.to_be_bytes());
+        bytes.extend_from_slice(&self.translation.z.to_be_bytes());
+        bytes.extend_from_slice(&self.rotation.x.to_be_bytes());
+        bytes.extend_from_slice(&self.rotation.y.to_be_bytes());
+        bytes.extend_from_slice(&self.rotation.z.to_be_bytes());
+        bytes.extend_from_slice(&self.rotation.w.to_be_bytes());
+        bytes.extend_from_slice(&self.anim_index.to_be_bytes());
+        bytes.extend_from_slice(&self.anim_timer.to_be_bytes());
+        bytes
+    }
+
+    pub fn from_bytes(data: &[u8]) -> Player {
+        let id = u32::from_be_bytes(data[0..4].try_into().unwrap());
+        let translation = gmm::Float3::new(
+            f32::from_be_bytes(data[4..8].try_into().unwrap()),
+            f32::from_be_bytes(data[8..12].try_into().unwrap()),
+            f32::from_be_bytes(data[12..16].try_into().unwrap()),
+        );
+        let rotation = gmm::Float4::new(
+            f32::from_be_bytes(data[16..20].try_into().unwrap()),
+            f32::from_be_bytes(data[20..24].try_into().unwrap()),
+            f32::from_be_bytes(data[24..28].try_into().unwrap()),
+            f32::from_be_bytes(data[28..32].try_into().unwrap()),
+        );
+        let anim_index = u32::from_be_bytes(data[32..36].try_into().unwrap());
+        let anim_timer = f32::from_be_bytes(data[36..40].try_into().unwrap());
+        Player {
+            id, 
+            translation, 
+            rotation, 
+            anim_index, 
+            anim_timer, 
+        }
+    }
+}
+
+impl Default for Player {
+    #[inline]
+    fn default() -> Self {
+        Self { 
+            id: 0, 
+            translation: gmm::Float3::ZERO, 
+            rotation: gmm::Float4::W, 
+            anim_index: 0, 
+            anim_timer: 0.0 
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,24 @@
-use std::mem::size_of;
-
-struct PacketType(u8);
-impl PacketType{
-    const RAW: Self = Self(0);
-    const MOVE: Self = Self(1);
-    const UPDATE: Self = Self(2);
-
-    // 크기가 u8보다 커지면 이 함수 활성화
-    // fn to_be_bytes(&self) -> [u8; size_of::<PacketType>()] {
-    //     self.0.to_be_bytes()
-    // }
-}
-
-type PacketSize = u16;
-
-#[repr(packed)]
-struct PacketHeader {
-    size: PacketSize,
-    packet_type: PacketType,
-}
-
-impl PacketHeader {
-    fn to_be_bytes(&self) -> [u8; size_of::<PacketHeader>()] {
-        let mut bytes = [0; size_of::<PacketHeader>()];
-        bytes[..size_of::<PacketSize>()].copy_from_slice(&self.size.to_be_bytes());
-        bytes[size_of::<PacketSize>()] = self.packet_type.0;
-        // bytes[size_of::<PacketSize>()..].copy_from_slice(&self.packet_type.to_be_bytes());   // 크기가 u8보다 커지면 이 코드 활성화
-        bytes
-    }
-}
-
-struct Packet {
-    header: PacketHeader,
-    data: Vec<u8>,
-}
-
-impl Packet {
-    fn new(data: &[u8]) -> Self {
-        Self {
-            header: PacketHeader {
-                size: (data.len() + std::mem::size_of::<PacketSize>()) as PacketSize,
-                packet_type: PacketType::RAW,
-            },
-            data: data.to_vec(),
-        }
-    }
-
-    fn from_bytes(bytes: &[u8]) -> Option<Self> {
-        if bytes.len() < size_of::<PacketHeader>() {
-            return None;
-        }
-
-        let header = PacketHeader {
-            size: PacketSize::from_be_bytes(bytes[..size_of::<PacketSize>()].try_into().unwrap()),
-            packet_type: PacketType(bytes[2]),
-        };
-
-        if bytes.len() < header.size as usize {
-            return None;
-        }
-
-        Some(Self {
-            header,
-            data: bytes[size_of::<PacketHeader>()..].to_vec(),
-        })
-    }
-
-    fn as_bytes(&self) -> Vec<u8> {
-        let mut bytes = Vec::with_capacity(self.header.size as usize);
-        bytes.extend_from_slice(&self.header.to_be_bytes());
-        bytes.extend_from_slice(&self.data);
-        bytes
-    }
-}
+use rust_lab::Player;
 
 
 fn main() {
-    let packet = Packet::new(&(0..16).collect::<Vec<u8>>());
+    let player = Player::new(3, gmm::Float3::ZERO, gmm::Float4::W, 2, 7.28);
+    
+    let start = std::time::Instant::now();
+    for _ in 0..10000000 {
+        let _bytes = player.as_bytes();
+    }
+    println!("as_bytes: {:?}", start.elapsed());
 
-    let bytes = packet.as_bytes();
-    println!("{:?}", bytes);
-    // [0, 18, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    // u16, type, data
-    // big-endian
+    let bytes = player.as_bytes();
 
     let start = std::time::Instant::now();
-    for _ in 0..1_000_000 {
-        packet.as_bytes();
+    for _ in 0..10000000 {
+        let _player_copy = Player::from_bytes(&bytes);
     }
-    println!("{:?}", start.elapsed());  // 250 ms
-
-    let start = std::time::Instant::now();
-    for _ in 0..1_000_000 {
-        Packet::from_bytes(&bytes).unwrap();
-    }
-    println!("{:?}", start.elapsed());  // 1.5 s
+    println!("from_bytes: {:?}", start.elapsed());
 }
+
+
+// bytemuck: 900ms, 150ms
+// be_bytes: 3.0s, 2.2s
