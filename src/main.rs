74e8399bae77437f11b5b9b92e01d859349765aa
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-10-08 14:27:12 +0900

    init

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,3 @@
-// use rust_lab::mpsc_server;
-use rust_lab::tokio_mpsc_server;
-// use rust_lab::lfqueue_server;
-
 fn main() {
-    let num_clients = 10;
-
-    // mpsc_server(num_clients);
-    tokio_mpsc_server(num_clients);
-    // lfqueue_server(num_clients);
     
-    // {
-        // use std::thread;
-        // let mpsc_handle = thread::spawn(move || {
-        //     mpsc_server(num_clients);
-        // });
-        // let lfqueue_handle = thread::spawn(move || {
-        //     lfqueue_server(num_clients);
-        // });
-
-        // mpsc_handle.join().unwrap();
-        // lfqueue_handle.join().unwrap();
-    // }
 }
