945c855b1345f5d205cba491ea8459a81494b661
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2024-10-08 17:36:43 +0900

    ?

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,30 @@ use std::ptr::addr_of;
 
 
 fn main() {
-    let queue = VecDeque::from([
+    let mut queue = VecDeque::from([
         1, 2, 3, 4, 5,
         6, 7, 8, 9, 10, 
-        11, 12, 13, 14, 15,
-        16, 17, 18, 19, 20
     ]);
+
+    queue.iter().for_each(|x| {
+        println!("value: {} \t addr: {:?}", x, addr_of!(*x));
+    });
+
+    queue.pop_front();
+    queue.pop_front();
+    queue.pop_front();
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
+    queue.push_front(11);
 
     queue.iter().for_each(|x| {
         println!("value: {} \t addr: {:?}", x, addr_of!(*x));
