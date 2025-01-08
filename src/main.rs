e567fca3ba1166e3aa1c38b7e81984574af2b3f7
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2025-01-08 15:33:17 +0900

    zip

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
 fn main() {
-    let v = (0..10).map(|x| 
-            (0..10).map(move |y| (x, y)).collect::<Vec<_>>()
-        ).collect::<Vec<_>>();
-    println!("{:?}", v);
-
-    let folded = v.iter().enumerate().fold(Vec::new(), |mut acc, x| {
-        if x.0 % 2 == 0 {
-            acc.extend(x.1.iter().cloned());
-        }
-        acc
-    });
-    println!("{:?}", folded);
+    println!("{:?}", (0..10).zip(20..30).collect::<Vec<_>>());
 }
