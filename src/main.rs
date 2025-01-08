f18552163b21b5f5257032bafc2547472bb6cfb6
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2025-01-08 13:56:54 +0900

    flatten

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,10 @@
-use std::collections::VecDeque;
-use std::ptr::addr_of;
-
-
 fn main() {
-    let mut queue = VecDeque::from([
-        1, 2, 3, 4, 5,
-        6, 7, 8, 9, 10, 
-    ]);
-
-    queue.iter().for_each(|x| {
-        println!("value: {} \t addr: {:?}", x, addr_of!(*x));
-    });
-
-    queue.pop_front();
-    queue.pop_front();
-    queue.pop_front();
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-    queue.push_front(11);
-
-    queue.iter().for_each(|x| {
-        println!("value: {} \t addr: {:?}", x, addr_of!(*x));
-    });
+    let v = (0..50)
+        .map(|x| 
+            (0..50).map(move |y| (x, y)).collect::<Vec<_>>()
+        ).collect::<Vec<_>>();
+    println!("{:?}", v);
+    
+    let v = v.iter().flatten().collect::<Vec<_>>();
+    println!("{:?}", v);
 }
